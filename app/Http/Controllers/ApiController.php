5adc6d0350b6bb2ba450d24ae305df5a80a446a6
root <root@tinybird.ca>
2017-10-15 23:27:21 +0000

    Added friendship capability

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\User;
+use App\Friend;
 use Illuminate\Support\Facades\Hash;
 use Validator;
 use GuzzleHttp\Client;
@@ -11,25 +12,32 @@ use GuzzleHttp\Client;
 class ApiController extends Controller
 {
 
+    /**
+     * Function to create a new user
+     * @param  Request $request     HTTP request being sent to the API.
+     * @return [JsonArray]          Return array including username, status, tokens.
+     */
     public function createUser(Request $request) {
 
+        //  If we have an empty email or password supplied, return an error message,
     	if(empty($request->input('email'))) 
     	{
     		//  Return empty email error
-    		return response()->json(['status' => '0', 'content' => 'ERROR: Please specify an email address.']);
+    		return response()->json(['status' => '0', 'content' => 'Please specify an email address.']);
     	}
     	else if(empty($request->input('password'))) {
     		//  Return empty password error
-    		return response()->json(['status' => '0', 'content' => 'ERROR: Please specify a password.']);
+    		return response()->json(['status' => '0', 'content' => 'Please specify a password.']);
     	}
 
-    	//  Build the validation array
+    	//  Build the validation array.  Email has to be unique in the users table
     	$input['email'] = $request->input('email');
     	$rules = array('email' => 'unique:users,email');
     	$validator = Validator::make($input, $rules);
 
+        //  If the validator fails, we have that email registered already, so return an error.
     	if ($validator->fails()) {
-    		return response()->json(['status' => '0', 'content' => 'ERROR: That email already exists.']);
+    		return response()->json(['status' => '0', 'content' => 'That email already exists.']);
 		}
 		else {
             //  Create the user
@@ -57,28 +65,40 @@ class ApiController extends Controller
 		}
     }
 
+
+    /**
+     * Function to log a user in.
+     * @param  Request $request     HTTP request being sent to the API
+     * @return JsonArray            Return object including status, user object and tokens.
+     */
     public function validateUser(Request $request) {
+
+        //  If we're supplied an empty email or password, send back error message.
         if(empty($request->input('email'))) 
         {
             //  Return empty email error
-            return response()->json(['status' => '0', 'content' => 'ERROR: Please specify an email address.']);
+            return response()->json(['status' => '0', 'content' => 'Please specify an email address.']);
         }
         else if(empty($request->input('password'))) {
             //  Return empty password error
-            return response()->json(['status' => '0', 'content' => 'ERROR: Please specify a password.']);
+            return response()->json(['status' => '0', 'content' => 'Please specify a password.']);
         }
 
-        //  Build the validation array
+        //  Build the validation array.  Email must be unique across users table
         $input['email'] = $request->input('email');
         $rules = array('email' => 'unique:users,email');
         $validator = Validator::make($input, $rules);
 
+        //  If the validator fails, we know the email exists, so check if the password works
         if ($validator->fails()) {
             //  If the validator fails, we know the email exists.  Now test if the PW works
             $thisuser = User::where('email', $request->input('email'))->first();
+
             if (Hash::check($request->input('password'), $thisuser->password)) {
+
                 //Generate a token for the user
                 $http = new Client;
+
                 $auth_response = $http->post('http://tabme.tinybird.ca/oauth/token', [
                     'form_params' => [
                         'grant_type' => 'password',
@@ -95,11 +115,49 @@ class ApiController extends Controller
                 return response()->json(['status' => '200', 'content' => $thisuser, 'access_token' => $tokens['access_token'], 'refresh_token' => $tokens['refresh_token']]);
             }
             else {
-                return response()->json(['status' => '0', 'content' => 'ERROR:  The password is incorrect.']);
+                return response()->json(['status' => '0', 'content' => 'The password is incorrect.']);
             }
         }
         else {
-            return response()->json(['status' => '0', 'content' => 'ERROR: That email does not exist.']);
+            return response()->json(['status' => '0', 'content' => 'That email is not registered with us.']);
         }
+    }
+
+    /**
+     * Add a friend for a specified user.
+     * @param Request $request     HTTP request sent to the API
+     */
+    function addFriend(Request $request) {
+
+        //  Get the variables
+        $email = $request->input('email');
+        $user_id = $request->input('user_id');
+
+        //  Check if the requested email exists
+        if(User::where('email', '=', $email)->count() > 0) {
+
+            //  Check if the friendship exists
+            $user_to_add = User::where('email', '=', $email)->first();
+            if(Friend::where([
+                ['friend_id', '=', $user_to_add->id],
+                ['user_id', '=', $user_id]
+            ])->count() == 0) {
+
+                //  Create the friendship
+                $newfriendship = new Friend;
+                $newfriendship->user_id = $user_id;
+                $newfriendship->friend_id = $user_to_add->id;
+                $newfriendship->save();
+
+                return response()->json(['status' => '200']);
+            }
+            else {
+                return response()->json(['status' => '0', 'content' => "You are already friends with this person."]);
+            }
+        }
+        else {
+            return response()->json(['status' => '0', 'content' => 'That email is not registered.']);
+        }
+
     }
 }
